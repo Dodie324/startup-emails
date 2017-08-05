2755544299e0b46bc28daf88606ab1a01c2ae986
Dodie Moore <dmoore@everquote.com>
2017-08-05 10:43:35 -0400

    Begin adding Google oAuth

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const keys = require("./config/keys");
+
 const app = express();
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.GOOGLE_CLIENT_ID,
+      clientSecret: keys.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {}
+  )
+);
+
 app.get(
-  "/",
-  (responseHandler = (req, res) => {
-    res.send({ hi: "there" });
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
   })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // dynamically decide which port to listen on
 // w/e heroku runs our app, it can inject environment variables.
