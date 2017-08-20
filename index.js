155be03229066685d6ab054d007e65219717d11d
Dodie Moore <dmoore@everquote.com>
2017-08-20 12:35:34 -0400

    Set up billing

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session"); //stores session data into the cookie; no need to install outside server to store a reference to the session like express-session
-const passport = require("passport");
-const keys = require("./config/keys");
-require("./models/User");
-require("./services/passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session'); //stores session data into the cookie; no need to install outside server to store a reference to the session like express-session
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
 /*
 interacts with mongoDB;
@@ -20,17 +21,20 @@ const app = express();
 Middlewares are small func that can be used to modifiy incoming requests to our app before they're sent off to route handlers
 */
 
+app.use(bodyParser.json());
+
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000, // how long it can exist in browser (30 Days in this case)
-    keys: [keys.COOKIE_KEY] // allows us to pass keys that can encrypt any cookie
-  })
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, // how long it can exist in browser (30 Days in this case)
+		keys: [keys.COOKIE_KEY] // allows us to pass keys that can encrypt any cookie
+	})
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/authRoutes")(app);
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 /* dynamically decide which port to listen on.
 whenever heroku runs our app, it can inject environment variables. it will decide which port we'll use in production, otherwise use 5000 in dev. */
