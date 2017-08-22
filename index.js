f3d97dd1928a3a0a4b048098fac5ebe440e6b245
Dodie Moore <dmoore@everquote.com>
2017-08-21 21:11:20 -0400

    deployment routing

--- index.js
+++ index.js
@@ -36,6 +36,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	// catch all case
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 /* dynamically decide which port to listen on.
 whenever heroku runs our app, it can inject environment variables. it will decide which port we'll use in production, otherwise use 5000 in dev. */
 
